862aa7615a253aa7c0c8115c7984a77189e167e9
Luis Angel Garrido <luangarso@icloud.com>
2025-10-24 14:01:17 -0400

    feat(migrations): create new media table and migrate data from old media table
    feat(utilities): add formatAuthors utility to prettify author names
    
    feat(utilities): add formatDateTime utility for formatting timestamps
    
    feat(utilities): implement generateMeta utility for generating metadata
    
    feat(utilities): create getGlobals utility for fetching global data with caching
    
    feat(utilities): add getMeUser utility for fetching user data with redirection
    
    feat(utilities): implement mergeOpenGraph utility for merging Open Graph metadata
    
    feat(utilities): add toKebabCase utility for converting strings to kebab case

---
+++ src/migrations/20251024_175622.ts
@@ -0,0 +1,103 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-d1-sqlite'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.run(sql`PRAGMA foreign_keys=OFF;`)
+  await db.run(sql`CREATE TABLE \`__new_media\` (
+  	\`id\` integer PRIMARY KEY NOT NULL,
+  	\`alt\` text,
+  	\`caption\` text,
+  	\`updated_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
+  	\`created_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
+  	\`url\` text,
+  	\`thumbnail_u_r_l\` text,
+  	\`filename\` text,
+  	\`mime_type\` text,
+  	\`filesize\` numeric,
+  	\`width\` numeric,
+  	\`height\` numeric,
+  	\`focal_x\` numeric,
+  	\`focal_y\` numeric,
+  	\`sizes_thumbnail_url\` text,
+  	\`sizes_thumbnail_width\` numeric,
+  	\`sizes_thumbnail_height\` numeric,
+  	\`sizes_thumbnail_mime_type\` text,
+  	\`sizes_thumbnail_filesize\` numeric,
+  	\`sizes_thumbnail_filename\` text,
+  	\`sizes_square_url\` text,
+  	\`sizes_square_width\` numeric,
+  	\`sizes_square_height\` numeric,
+  	\`sizes_square_mime_type\` text,
+  	\`sizes_square_filesize\` numeric,
+  	\`sizes_square_filename\` text,
+  	\`sizes_small_url\` text,
+  	\`sizes_small_width\` numeric,
+  	\`sizes_small_height\` numeric,
+  	\`sizes_small_mime_type\` text,
+  	\`sizes_small_filesize\` numeric,
+  	\`sizes_small_filename\` text,
+  	\`sizes_medium_url\` text,
+  	\`sizes_medium_width\` numeric,
+  	\`sizes_medium_height\` numeric,
+  	\`sizes_medium_mime_type\` text,
+  	\`sizes_medium_filesize\` numeric,
+  	\`sizes_medium_filename\` text,
+  	\`sizes_large_url\` text,
+  	\`sizes_large_width\` numeric,
+  	\`sizes_large_height\` numeric,
+  	\`sizes_large_mime_type\` text,
+  	\`sizes_large_filesize\` numeric,
+  	\`sizes_large_filename\` text,
+  	\`sizes_xlarge_url\` text,
+  	\`sizes_xlarge_width\` numeric,
+  	\`sizes_xlarge_height\` numeric,
+  	\`sizes_xlarge_mime_type\` text,
+  	\`sizes_xlarge_filesize\` numeric,
+  	\`sizes_xlarge_filename\` text,
+  	\`sizes_og_url\` text,
+  	\`sizes_og_width\` numeric,
+  	\`sizes_og_height\` numeric,
+  	\`sizes_og_mime_type\` text,
+  	\`sizes_og_filesize\` numeric,
+  	\`sizes_og_filename\` text
+  );
+  `)
+  await db.run(sql`INSERT INTO \`__new_media\`("id", "alt", "caption", "updated_at", "created_at", "url", "thumbnail_u_r_l", "filename", "mime_type", "filesize", "width", "height", "focal_x", "focal_y", "sizes_thumbnail_url", "sizes_thumbnail_width", "sizes_thumbnail_height", "sizes_thumbnail_mime_type", "sizes_thumbnail_filesize", "sizes_thumbnail_filename", "sizes_square_url", "sizes_square_width", "sizes_square_height", "sizes_square_mime_type", "sizes_square_filesize", "sizes_square_filename", "sizes_small_url", "sizes_small_width", "sizes_small_height", "sizes_small_mime_type", "sizes_small_filesize", "sizes_small_filename", "sizes_medium_url", "sizes_medium_width", "sizes_medium_height", "sizes_medium_mime_type", "sizes_medium_filesize", "sizes_medium_filename", "sizes_large_url", "sizes_large_width", "sizes_large_height", "sizes_large_mime_type", "sizes_large_filesize", "sizes_large_filename", "sizes_xlarge_url", "sizes_xlarge_width", "sizes_xlarge_height", "sizes_xlarge_mime_type", "sizes_xlarge_filesize", "sizes_xlarge_filename", "sizes_og_url", "sizes_og_width", "sizes_og_height", "sizes_og_mime_type", "sizes_og_filesize", "sizes_og_filename") SELECT "id", "alt", "caption", "updated_at", "created_at", "url", "thumbnail_u_r_l", "filename", "mime_type", "filesize", "width", "height", "focal_x", "focal_y", "sizes_thumbnail_url", "sizes_thumbnail_width", "sizes_thumbnail_height", "sizes_thumbnail_mime_type", "sizes_thumbnail_filesize", "sizes_thumbnail_filename", "sizes_square_url", "sizes_square_width", "sizes_square_height", "sizes_square_mime_type", "sizes_square_filesize", "sizes_square_filename", "sizes_small_url", "sizes_small_width", "sizes_small_height", "sizes_small_mime_type", "sizes_small_filesize", "sizes_small_filename", "sizes_medium_url", "sizes_medium_width", "sizes_medium_height", "sizes_medium_mime_type", "sizes_medium_filesize", "sizes_medium_filename", "sizes_large_url", "sizes_large_width", "sizes_large_height", "sizes_large_mime_type", "sizes_large_filesize", "sizes_large_filename", "sizes_xlarge_url", "sizes_xlarge_width", "sizes_xlarge_height", "sizes_xlarge_mime_type", "sizes_xlarge_filesize", "sizes_xlarge_filename", "sizes_og_url", "sizes_og_width", "sizes_og_height", "sizes_og_mime_type", "sizes_og_filesize", "sizes_og_filename" FROM \`media\`;`)
+  await db.run(sql`DROP TABLE \`media\`;`)
+  await db.run(sql`ALTER TABLE \`__new_media\` RENAME TO \`media\`;`)
+  await db.run(sql`PRAGMA foreign_keys=ON;`)
+  await db.run(sql`CREATE INDEX \`media_updated_at_idx\` ON \`media\` (\`updated_at\`);`)
+  await db.run(sql`CREATE INDEX \`media_created_at_idx\` ON \`media\` (\`created_at\`);`)
+  await db.run(sql`CREATE UNIQUE INDEX \`media_filename_idx\` ON \`media\` (\`filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_thumbnail_sizes_thumbnail_filename_idx\` ON \`media\` (\`sizes_thumbnail_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_square_sizes_square_filename_idx\` ON \`media\` (\`sizes_square_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_small_sizes_small_filename_idx\` ON \`media\` (\`sizes_small_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_medium_sizes_medium_filename_idx\` ON \`media\` (\`sizes_medium_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_large_sizes_large_filename_idx\` ON \`media\` (\`sizes_large_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_xlarge_sizes_xlarge_filename_idx\` ON \`media\` (\`sizes_xlarge_filename\`);`)
+  await db.run(sql`CREATE INDEX \`media_sizes_og_sizes_og_filename_idx\` ON \`media\` (\`sizes_og_filename\`);`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.run(sql`PRAGMA foreign_keys=OFF;`)
+  await db.run(sql`CREATE TABLE \`__new_media\` (
+  	\`id\` integer PRIMARY KEY NOT NULL,
+  	\`alt\` text NOT NULL,
+  	\`updated_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
+  	\`created_at\` text DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ', 'now')) NOT NULL,
+  	\`url\` text,
+  	\`thumbnail_u_r_l\` text,
+  	\`filename\` text,
+  	\`mime_type\` text,
+  	\`filesize\` numeric,
+  	\`width\` numeric,
+  	\`height\` numeric
+  );
+  `)
+  await db.run(sql`INSERT INTO \`__new_media\`("id", "alt", "updated_at", "created_at", "url", "thumbnail_u_r_l", "filename", "mime_type", "filesize", "width", "height") SELECT "id", "alt", "updated_at", "created_at", "url", "thumbnail_u_r_l", "filename", "mime_type", "filesize", "width", "height" FROM \`media\`;`)
+  await db.run(sql`DROP TABLE \`media\`;`)
+  await db.run(sql`ALTER TABLE \`__new_media\` RENAME TO \`media\`;`)
+  await db.run(sql`PRAGMA foreign_keys=ON;`)
+  await db.run(sql`CREATE INDEX \`media_updated_at_idx\` ON \`media\` (\`updated_at\`);`)
+  await db.run(sql`CREATE INDEX \`media_created_at_idx\` ON \`media\` (\`created_at\`);`)
+  await db.run(sql`CREATE UNIQUE INDEX \`media_filename_idx\` ON \`media\` (\`filename\`);`)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250929_111647 from './20250929_111647';
 import * as migration_20251024_174217 from './20251024_174217';
+import * as migration_20251024_175622 from './20251024_175622';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20251024_174217.up,
     down: migration_20251024_174217.down,
-    name: '20251024_174217'
+    name: '20251024_174217',
+  },
+  {
+    up: migration_20251024_175622.up,
+    down: migration_20251024_175622.down,
+    name: '20251024_175622'
   },
 ];
